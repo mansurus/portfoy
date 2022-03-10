d26b8c55214a72b8cbca320b69f957ba0335a0dc
mansurus <mansurusia@gmail.com>
2022-03-11 00:07:32 +0300

    item add item remove added

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,33 +1,89 @@
 import React, { Component } from 'react'
-import {  FormGroup, Input} from 'reactstrap';
+import { FormGroup, Input } from 'reactstrap';
 import { RiDragMove2Fill } from 'react-icons/ri';
-import {   BsPlus,BsSquareFill } from 'react-icons/bs';
+import { BsPlus, BsSquareFill } from 'react-icons/bs';
 import { MdOutlineClose } from 'react-icons/md';
 import './index.css'
 
-export  class AddItem extends Component {
-  render() {
-    return (
-        <div class="flex-container">
-          
-        <RiDragMove2Fill size="2em" class="icons-align"/> <FormGroup class="item" style={{width:'10%'}} >
-            <Input type="select" name="select" id="exampleSelect">
-              <option>1</option>
-              <option>2</option>            
-            </Input>
-          </FormGroup>
-          <FormGroup class="item" style={{width:'7%'}}>
-            <Input type="text" name="text" id="exampleText" placeholder="text" />
-          </FormGroup>
-          <FormGroup class="item" style={{width:'7%'}}>
-            <Input type="text" name="text2" id="exampleText2" placeholder="text" />
-          </FormGroup>
-          <FormGroup class="item" style={{width:'40%'}}>
-            <Input type="text" name="text3" id="exampleText3" placeholder="text" />
-          </FormGroup>
-          <MdOutlineClose class='icons-align' size='2em' color='red' />
-          <BsPlus class='icons-align' size='2em' color='green' />
-        </div>
-    )
-  }
+export class AddItem extends Component
+{
+	removeItem(item)
+	{
+		//this.props.updateItem(this.props.value.id, index);
+		this.props.removeItem(this.props.value.id);
+	}
+
+	removeSubItem(index)
+	{
+		this.props.removeSubItem(this.props.value.id, index);
+	}
+
+	addSubItem()
+	{
+		this.props.addSubItem(this.props.value.id);
+	}
+
+	render()
+	{
+		return (
+			<div>
+				<div className="flex-container">
+					<div style={{ marginTop: "-10px", width: '7%' }}>
+						<div style={{ background: "#1fb3d1", padding: "1px", margin: "10px" }}><RiDragMove2Fill size="2em" className="icons-align" color='white' /></div>
+					</div>
+
+					<FormGroup className="item" style={{ width: '10%' }} disabled={true}>
+						<Input type="select" name="select" id="exampleSelect" disabled={true}>
+							<option>1</option>
+							<option>2</option>
+						</Input>
+					</FormGroup>
+					<FormGroup className="item" style={{ width: '7%' }}>
+						<Input type="text" name="text" id="exampleText" placeholder="gun" value={this.props.value.gun} />
+					</FormGroup>
+					<FormGroup className="item" style={{ width: '7%' }}>
+						<Input type="text" name="text2" id="exampleText2" placeholder="saat" value={this.props.value.saat} />
+					</FormGroup>
+					<FormGroup className="item" style={{ width: '40%' }}>
+						<Input type="text" name="text3" id="exampleText3" placeholder="egitimYontemi" value={this.props.value.egitimYontemi} />
+					</FormGroup>
+					<div style={{ marginTop: "-10px", width: '7%' }}>
+						<div style={{ background: "#d15656", padding: "1px", margin: "10px" }} onClick={() => this.removeItem(this.props.value)}>
+							<MdOutlineClose className='icons-align' size='2em' color='white' />
+						</div>
+					</div>
+					<div style={{ marginTop: "-10px", width: '7%' }}>
+						<div style={{ background: "#40b76f", padding: "1px", margin: "10px" }} onClick={() => this.addSubItem()}>
+							<BsPlus className='icons-align' size='2em' color='white' />
+						</div>
+					</div>
+				</div>
+				<div>
+					{this.props.value.items.map((item, index) => <div key={item.id} className="flex-container">
+						<div style={{ width: '6%' }}>&nbsp;</div>
+						<FormGroup className="item" style={{ width: '10%' }} >
+							<Input type="select" name="select" id="exampleSelect">
+								<option>1</option>
+								<option>2</option>
+							</Input>
+						</FormGroup>
+						<div style={{ width: '7%' }}>&nbsp;</div>
+						<FormGroup className="item" style={{ width: '7%' }}>
+							<Input type="text" name="text2" id="exampleText2" placeholder="text" defaultValue={item.saat} />
+						</FormGroup>
+						<FormGroup className="item" style={{ width: '40%', whiteSpace: "nowrap" }}>
+							<Input type="text" name="text3" id="exampleText3" placeholder="text" defaultValue={item.egitimYontemi} style={{ display: "inline-block", width: "88%" }} />
+							<div style={{ marginTop: "-10px", display: "inline-block" }}>
+								<div style={{ background: "#d15656", padding: "1px", margin: "10px" }} onClick={() => this.removeSubItem(index)}>
+									<MdOutlineClose className='icons-align' size='2em' color='white' />
+								</div>
+							</div>
+						</FormGroup>
+						<div style={{ width: '7%' }}>&nbsp;</div>
+						<div style={{ width: '7%' }}>&nbsp;</div>
+					</div>)}
+				</div>
+			</div>
+		)
+	}
 }
--- src/PortfolioDetail.js
+++ src/PortfolioDetail.js
@@ -1,85 +1,136 @@
 import React, { Component, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button,Label } from "reactstrap";
+import { Button, Label } from "reactstrap";
 import { BsArrowRight } from "react-icons/bs";
 import "./index.css";
 import { AddItem } from "./AddItem";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-const liste = [{ id: "1"}, { id: "2" }, { id: "3" }];
+const liste = [
+	{ id: "1", gun: 1, saat: 7, egitimYontemi: "Ticari Kredi Mevzuatı - Giriş", items: [] },
+	{
+		id: "2", gun: 2, saat: 4, egitimYontemi: "Def", items: [
+			{ id: "2.1", ayniGun: 1, saat: 2, egitimYontemi: "aa" },
+			{ id: "2.2", ayniGun: 1, saat: 2, egitimYontemi: "bb" },
+			{ id: "2.3", ayniGun: 1, saat: 2, egitimYontemi: "cc" }
+		]
+	},
+	{ id: "3", gun: 3, saat: 5, egitimYontemi: "Xyz", items: [] }
+];
 
+function PortfolioDetail()
+{
+	const [finalListe, updatedListe] = useState(liste);
 
-function PortfolioDetail() {
-    const [finalListe, updatedListe] = useState(liste);
+	function handleOnDragEnd(result)
+	{
+		if (!result.destination)
+			return;
+		const items = Array.from(finalListe);
+		const [reorderedItem] = items.splice(result.source.index, 1);
+		items.splice(result.destination.index, 0, reorderedItem);
 
-    function handleOnDragEnd(result) {
-        if (!result.destination) return;
-        const items = Array.from(finalListe);
-        const [reorderedItem] = items.splice(result.source.index,1);
-        items.splice(result.destination.index, 0, reorderedItem);
+		updatedListe(items);
+	}
 
-        updatedListe(items);
-    }
-  
-    return (
-      <div style={{ marginTop: "20vh" }}>
-        <div className="header">
-          <p>This is some content in sticky footer</p>
-        </div>
-        <h1
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          Eğitim Yöntemi Çizelge Tanımları{" "}
-        </h1>
-        <div class="flex-container">
-        <Label for="exampleSelect">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Label>
-        <Label for="exampleSelect" style={{width:'8%'}}>Aynı Gün</Label>
-        <Label for="exampleText" style={{width:'6%'}}>Gün</Label>
-        <Label for="exampleText2" style={{width:'6%'}}>Saat</Label>
-        <Label for="exampleText3" style={{width:'41%'}}>Eğitim Yönetimi</Label>
-        <Label for="exampleSelect">&nbsp;&nbsp;</Label>
-        <Label for="exampleSelect">&nbsp;&nbsp; </Label>
-          </div>
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="liste">
-            {(provided) => (
-              <ul
-                className="listStyle"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {finalListe.map(({ id }, index) => {
-                  return (
-                    <Draggable key={id} draggableId={id} index={index}>
-                      {(provided) => (
-                        <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                          <AddItem />
-                        </li>
-                      )}
-                    </Draggable>
-                  );
-                })}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
+	function addSubItem(id)
+	{
+		const items = Array.from(finalListe);
+		for (let i of items)
+		{
+			if (i.id === id)
+				i.items.push({ id: id + "." + i.items.length.toString(), ayniGun: 0, saat: 0, egitimYontemi: "" });
+		}
 
-        <div className="footer">
-          <div style={{ display: "flex", justifyContent: "right" }}>
-            <Link to="/PortfolioDetail">
-              <Button>
-                Deneme Button <BsArrowRight />
-              </Button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    );
-  }
+		updatedListe(items);
+	}
+
+	function removeSubItem(id, index)
+	{
+		const items = Array.from(finalListe);
+		for (let i of items)
+		{
+			if (i.id === id)
+				i.items.splice(index, 1);
+		}
+
+		updatedListe(items);
+	}
+
+	function removeItem(id)
+	{
+		const items = Array.from(finalListe);
+
+		
+		for( var i = 0; i < items.length; i++)
+		{ 
+			if ( items[i].id === id)
+				items.splice(i, 1); 
+		}
+
+		updatedListe(items);
+	}
+
+	return (
+		<div style={{ marginTop: "20vh" }}>
+			<div className="header">
+				<p>This is some content in sticky footer</p>
+			</div>
+			<h1
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+				}}
+			>
+				Eğitim Yöntemi Çizelge Tanımları{" "}
+			</h1>
+			<div className="flex-container">
+				<Label for="exampleSelect">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Label>
+				<Label for="exampleSelect" style={{ width: '8%' }}>Aynı Gün</Label>
+				<Label for="exampleText" style={{ width: '6%' }}>Gün</Label>
+				<Label for="exampleText2" style={{ width: '6%' }}>Saat</Label>
+				<Label for="exampleText3" style={{ width: '41%' }}>Eğitim Yönetimi</Label>
+				<Label for="exampleSelect">&nbsp;&nbsp;</Label>
+				<Label for="exampleSelect">&nbsp;&nbsp; </Label>
+			</div>
+			<DragDropContext onDragEnd={handleOnDragEnd}>
+				<Droppable droppableId="liste">
+					{(provided) => (
+						<ul
+							className="listStyle"
+							{...provided.droppableProps}
+							ref={provided.innerRef}
+						>
+							{finalListe.map((item, index) =>
+							{
+								return (
+									<Draggable key={item.id} draggableId={item.id} index={index}>
+										{(provided) => (
+											<li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+												<AddItem value={item} addSubItem={addSubItem} removeSubItem={removeSubItem} removeItem={removeItem} />
+											</li>
+										)}
+									</Draggable>
+								);
+							})}
+							{provided.placeholder}
+						</ul>
+					)}
+				</Droppable>
+			</DragDropContext>
+
+			<div className="footer">
+				<div style={{ display: "flex", justifyContent: "right" }}>
+					<Link to="/PortfolioDetail">
+						<Button>
+							Deneme Button <BsArrowRight />
+						</Button>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+}
 
 export default PortfolioDetail;
